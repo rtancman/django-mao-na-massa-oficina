0d3c7c114e3faeb4b79d4ceafcff7473bb4d0455
Raffael Tancman <rtancman@gmail.com>
2023-05-19 17:33:47 -0300

    Add model Carro

--- oficina/core/models.py
+++ oficina/core/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Carro(models.Model):
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=200)
+    placa = models.CharField(max_length=200)
+    montadora = models.TextField()
+    data_criacao = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.nome
